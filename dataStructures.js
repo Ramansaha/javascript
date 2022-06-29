8603446ec03329672d15b029ef982be4c8e0d2ff
Raman <ramansaha37@gmail.com>
2022-06-30 02:28:50 +0530

    machineTest1

--- dataStructures.js
+++ dataStructures.js
@@ -1,29 +1,29 @@
 // level 2
 // 2. Replace all the vowels in a string with uppercase vowels.
 // Input string: 'Elie'
-// let myString = "Elie";
-// let temp;
-// let vowelStr = "aeiouAEIOU";
-// for (var i = 0; i < myString.length; i++)
-// {
-//     if(vowelStr.includes(myString[i]))
-//     {
-//         if (!temp)
-//         {
-//             temp = myString.substring(0,i) + myString.charAt(i).toUpperCase();
+let myString = "Elie";
+let temp;
+let vowelStr = "aeiouAEIOU";
+for (var i = 0; i < myString.length; i++)
+{
+    if(vowelStr.includes(myString[i]))
+    {
+        if (!temp)
+        {
+            temp = myString.substring(0,i) + myString.charAt(i).toUpperCase();
         
-//         }
-//         else
-//         {
-//             temp = temp + myString.charAt(i).toUpperCase();
-//         }
+        }
+        else
+        {
+            temp = temp + myString.charAt(i).toUpperCase();
+        }
          
-//     }
-//     else {
-//         temp = temp + myString.charAt(i);
-// }
-// }
-// console.log(temp);
+    }
+    else {
+        temp = temp + myString.charAt(i);
+}
+}
+console.log(temp);
 
 // Write a function to remove all even numbers from an Array.
 // Input array: 
@@ -31,11 +31,11 @@
 // Output should be array: [1, 3, 7]
 
 
-// function removeEven(numbers){
-// return numbers.filter(n => n % 2 !== 0)
-// }
-// const oddNumbers = removeEven( [1, 3, 4, 6, 7, 8]);
-// console.log(oddNumbers);
+function removeEven(numbers){
+return numbers.filter(n => n % 2 !== 0)
+}
+const oddNumbers = removeEven( [1, 3, 4, 6, 7, 8]);
+console.log(oddNumbers);
  
 // Write a function to find the maximum number in an array.
 // Input array: [1, 3, 4, 6, 7, 8, 2, 5]

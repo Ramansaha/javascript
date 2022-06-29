c4c2ffe47325e2ebcfc52b057c38067952a69bf5
Raman <ramansaha37@gmail.com>
2022-06-30 02:34:55 +0530

    machineTest1

--- codeQuality.js
+++ codeQuality.js
@@ -3,6 +3,9 @@
 async function getUsers() {
     let doc = await Promise.all(getUser(), getProfile(), getPosts());
   
+    //directly assigning values to the object keys rather than storing them in a variable from the array and then 
+    //assigning to the object from that,and creating unnccessary variables,so to overcome that extra usage of space 
+    //assigning it directly from the array
     const userProfile = {
       user: doc[0],
       profile: doc[1],
